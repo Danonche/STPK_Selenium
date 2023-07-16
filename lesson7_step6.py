422ae39e8a8a1cec343c2cc80e82113ea7147a3f
Danonche <larion.dli92@gmail.com>
2023-07-16 07:30:02 +0300

    Метод get_attribute

---
+++ lesson7_step6.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+    
+
+try:
+    link = "https://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+        
+    new_atribute = browser.find_element(By.ID, "treasure")
+    
+    new_atr2 = new_atribute.get_attribute("valuex")
+    
+    x = new_atr2
+    y = calc(x)
+
+    input1 = browser.find_element(By.CSS_SELECTOR, "#answer")
+    input1.send_keys(y)
+        
+    option1 = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox")
+    option1.click()
+        
+    option12 = browser.find_element(By.CSS_SELECTOR, "#robotsRule")
+    option12.click()
+        
+    button = browser.find_element(By.CSS_SELECTOR, ".btn")
+    button.click()
+finally:
+  # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
